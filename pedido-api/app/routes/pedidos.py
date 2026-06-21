6f0e7439a556ed836d767da02eea2d813f5ff3a6
solraC09 <coelhocadu0909@gmail.com>
2026-06-21 10:42:34 -0300

    feat: implementa endpoints de cadastro e listagem de pedidos

---
+++ pedido-api/app/routes/pedidos.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, status
+
+from app.database import pedidos_collection
+from app.models import Pedido
+from app.schemas import PedidoCreate
+
+from app.rabbitmq import publicar_pedido_criado
+from app.kafka_producer import publicar_evento_pedido_criado
+
+router = APIRouter(tags=["Pedidos"])
+
+
+@router.post("/pedidos", status_code=status.HTTP_201_CREATED)
+def criar_pedido(dados: PedidoCreate):
+
+    pedido = Pedido.criar(
+        cliente=dados.cliente,
+        produto=dados.produto,
+        quantidade=dados.quantidade
+    )
+
+    pedidos_collection.insert_one(pedido)
+
+    publicar_pedido_criado(pedido["id"])
+
+    publicar_evento_pedido_criado(
+        pedido["id"],
+        pedido["cliente"]
+    )
+
+    return pedido
+
+
+@router.get("/pedidos")
+def listar_pedidos():
+
+    pedidos = []
+
+    for pedido in pedidos_collection.find({}, {"_id": 0}):
+        pedidos.append(pedido)
+
+    return pedidos
